b14ce108100101587ce01aa4db1d83b710ccc577
saksham762007 <samrathore07062007@gmail.com>
2026-02-12 01:11:59 +0530

    file update

--- superman/src/App.jsx
+++ superman/src/App.jsx
@@ -1,5 +1,19 @@
-function App(){
-  return 'hello'
+import React from 'react'
+
+const App = () => {
+  return (
+
+    // fragments
+    <>
+    <div id='dad'>
+      App
+      </div>
+
+      <div id='chacha'>
+        <h1>sam</h1>
+      </div>
+    </>
+  )
 }
 
 export default App
