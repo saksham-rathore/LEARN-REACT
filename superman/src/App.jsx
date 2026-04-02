6a219c7da7cc4caa2c0eee1ca3743500fb87369d
saksham762007 <samrathore07062007@gmail.com>
2026-04-02 17:16:55 +0530

    feat: initialize React project with Vite and Tailwind CSS configuration

--- superman/src/App.jsx
+++ superman/src/App.jsx
@@ -1,45 +1,46 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [Add, setAdd] = useState("");
   const [Tasks, setTasks] = useState([]);
+  const [AddTask, setAddTask] = useState("");
 
-  // Add Task
-  const addtask = () => {
-    if (Add === "") return;
-
-    setTasks([...Tasks, Add]); // 🔥 state update
-    setAdd(""); // input clear
+  const addTask = () => {
+    setTasks([...tasks, task]);
   };
 
   return (
-    <div className="container">
-      <h1>To-Do List</h1>
-
-      <div className="input-section">
-        <input
-          type="text"
-          placeholder="Enter a new task"
-          value={Add}
-          onChange={(e) => setAdd(e.target.value)}
-        />
-
-        <button onClick={addtask} className="add-btn">
-          Add Task
-        </button>
-      </div>
-
-      {/* ✅ Dynamic List */}
-      <div className="task-list">
-        {Tasks.map((task, index) => (
-          <div className="task" key={index}>
-            <input type="checkbox" />
-            <span>{task}</span>
-            <button className="delete-btn">Delete</button>
+    <>
+      <div className="flex justify-center items-center mt-15">
+        <div className="h-200 w-200 bg-blue-100 bg-gradient-to-br from-slate-400 to-blue rounded-xl">
+          <div className="mx-auto mt-25 h-150 w-150 bg-gradient-to-br from-slate-500 to-blue rounded-xl shadow-2xl border-2xl">
+            <div className="items-center justify-center text-center">
+              <input
+                type="text"
+                placeholder="Search notes..."
+                className="border-2 rounded-xl w-140 h-10 mt-10"
+              />
+              <div>
+                <input
+                  type="text"
+                  placeholder="Enter your note"
+                  onChange={(e) => setTasks(e.target.value)}
+                  className="border-2 rounded-xl w-100 mt-10 h-10"
+                />
+                <button
+                  className="border-2 rounded-xl h-10 mx-10 w-30"
+                >
+                  Add Note
+                </button>
+              </div>
+              <div className="border-2 rounded-xl w-100 mt-10 h-10">
+                {AddTask}
+              </div>
+            </div>
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
