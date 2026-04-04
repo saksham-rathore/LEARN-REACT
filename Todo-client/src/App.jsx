614bc640ba9458dfbb2070b11b4ed5fbd6dba8ff
saksham762007 <samrathore07062007@gmail.com>
2026-04-04 12:15:38 +0530

    feat: initialize App component and TodoForm with context-based todo management logic

--- Todo-client/src/App.jsx
+++ Todo-client/src/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { TodoProvider } from "./Context/ContextTodo";
+import { useState } from "react";
 
 function App() {
+  const [todos, settodos] = useState([])
+
+  const addTodo = (Todo) => {
+    settodos((prev) => [{id: Date.now(), ...Todo} ,...prev])
+  }
+
+  const updateTodo = (Todo, id) => {
+    settodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? Todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    settodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id))
+  }
+
+  const CompleteTodo = (id) => {
+    settodos((prev) => prev.map((prevTodo) => (prevTodo === id ? {...prevTodo, Complete: !prevTodo.Complete} : prevTodo)))
+  }
 
   return (
     
-    <TodoProvider>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, CompleteTodo}}>
     <div className="bg-[#172842] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">
--- Todo-client/src/Components/TodoForm.jsx
+++ Todo-client/src/Components/TodoForm.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { useTodo } from "../../../todo-list/src/Context";
+import { useState } from "react";
+
 
 function TodoForm() {
+  const [Todo, setTodo] = useState("")
+  const {addTodo} = useTodo()
+
+  const add = (e) => {
+    e.preventdefauld()
+
+    if (!Todo) return
+
+    addTodo({Todo, completed: false})
+    setTodo("")
+  }
+
+
   return (
     <form className="flex">
       <input
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={add}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button
-        type="submit"
+        type="submit" 
         className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
       >
         Add
